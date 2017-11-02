9dc48e0ff39ad6d4f5689bc66e8f12787ec9d0b5
Antoine Hedgecock <antoine.hedgecock@gmail.com>
2017-11-02 16:03:39 +0200

    Handle rejections better by calling newrelic_notice_error with the exception

--- src/Events/NewrelicBackgroundTransaction.php
+++ src/Events/NewrelicBackgroundTransaction.php
@@ -11,6 +11,7 @@ namespace InteractiveSolutions\Bernard\Events;
 
 use Bernard\BernardEvents;
 use Bernard\Event\EnvelopeEvent;
+use Bernard\Event\RejectEnvelopeEvent;
 use InteractiveSolutions\Bernard\Message\AbstractExplicitMessage;
 
 class NewrelicBackgroundTransaction
@@ -30,7 +31,10 @@ class NewrelicBackgroundTransaction
         }
     }
 
-    public function terminate()
+    /**
+     * This is called after a transaction has "successfully" finished
+     */
+    public function acknowledge()
     {
         if (!function_exists('newrelic_end_transaction')) {
             return;
@@ -40,14 +44,35 @@ class NewrelicBackgroundTransaction
     }
 
     /**
+     * Reject is called when a task throws an exception
+     *
+     * @param RejectEnvelopeEvent $event
+     */
+    public function reject(RejectEnvelopeEvent $event)
+    {
+        if (!function_exists('newrelic_end_transaction'))  {
+            return;
+        }
+
+        $message = $event->getEnvelope()->getMessage();
+        if ($message instanceof AbstractExplicitMessage) {
+            newrelic_notice_error($message->getName(), $event->getException());
+            newrelic_end_transaction();
+        } else {
+            newrelic_notice_error($message->getName());
+            newrelic_end_transaction();
+        }
+    }
+
+    /**
      * @return array
      */
     public static function getSubscribedEvents()
     {
         return [
-            BernardEvents::INVOKE => ['onInvoke'],
-            BernardEvents::ACKNOWLEDGE => ['terminate'],
-            BernardEvents::REJECT => ['terminate']
+            BernardEvents::INVOKE => ['begin'],
+            BernardEvents::ACKNOWLEDGE => ['acknowledge'],
+            BernardEvents::REJECT => ['reject']
         ];
     }
 }
